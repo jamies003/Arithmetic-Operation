4e8876cf14bf908db692abbab31d755018296994
Jamie Stewart <stwjme@gmail.com>
2020-07-07 19:36:29 -0400

    conditionalstatements

---
+++ conditionalstatement.js
@@ -0,0 +1,20 @@
+function getLetter(s) {
+  let letter;
+  // Write your code here
+  switch (true) {
+      case /^[aeiou]/.test(s):
+      letter = "A";
+      break;
+      case /^[bcdfg]/.test(s):
+      letter = "B";
+      break;
+      case /^[hjklm]/.test(s):
+      letter = "C";
+      break;
+      case /^[npqrstvwxyz]/.test(s):
+      letter = "D";
+      break;
+
+  }
+  return letter;
+}
